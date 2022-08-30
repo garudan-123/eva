543c88519aa04221d3efc5408149eb6616afc6ce
Vinesh <92975459+Vineshvinu@users.noreply.github.com>
2022-08-30 18:40:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Vineshvinu/VIPautofilter.git /VIPautofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /VIPautofilter
 fi
-cd /EvaMaria
+cd /VIPautofilter 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
