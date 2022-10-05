0809e5ff320c92f64fd072ad9aabc9a3cc8272eb
joelthomas12 <108505636+joelthomas12@users.noreply.github.com>
2022-10-05 08:41:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vineshvinu/VIPautofilter.git /VIPautofilter
-else
+  git clone https://github.com/joelthomas12/VIPautofilter else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VIPautofilter
 fi
